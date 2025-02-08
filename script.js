e316f410c87c3c88f4ee9cbb4f6edebba6091a06
MatiasCardoso4 <cardosomatias1994@gmail.com>
2025-02-07 21:39:24 -0300

    Add outdoor image, enhance post creation with dynamic image handling, and improve CSS for image display

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 const posts = document.querySelector(".posts-container");
 
+const images = [
+  {
+    url: "./assets/cat.jpg",
+    alt: "A kitten saw something that capt his atention",
+  },
+  {
+    url: "./assets/man.jpg",
+    alt: "A man seeing a big mountain with snow and trees",
+  },
+  {
+    url: "./assets/outdoors.jpg",
+    alt: "A man that is looking yo the camera",
+  },
+];
+
 async function getUsers() {
   try {
-    const URL = "https://randomuser.me/api/?results=10";
+    const URL = "https://randomuser.me/api/?results=3";
     const response = await fetch(URL);
     const data = await response.json();
     setDisplay(data);
     return data;
   } catch (e) {
-    throw new Error(e)
+    throw new Error(e);
   }
 }
 
 const createElements = (person) => {
+  const { postHeader, post, image_post } = createPost(person);
+  const buttons = createButtons();
+
+  const totalLikes = document.createElement("p");
+  totalLikes.classList.add("total-likes");
+  totalLikes.textContent = `${Math.floor(Math.random() * 100)} likes`;
+
+  const div = document.createElement("div");
+  div.className = "username-photo-title";
+  const user_name = document.createElement("p");
+  user_name.textContent = `${person.login.username}`;
+
+  const title = document.createElement("p");
+  title.textContent = "My adorable kitten";
+  title.classList.add("commentary");
+  div.append(user_name, title);
+
+  user_name.classList.add("user-name");
+
+  post.append(postHeader, image_post, buttons, totalLikes, div);
+
+  posts.appendChild(post);
+};
+
+function createPost(person) {
+  const image_post = createImagePost();
+
   /*Post Container*/
   const post = document.createElement("section");
   post.className = "post";
@@ -23,13 +65,13 @@ const createElements = (person) => {
   const userInfo = document.createElement("div");
   const avatar = document.createElement("img");
   const name = document.createElement("p");
-  //   const locationContainer = document.createElement("div");
+  // const locationContainer = document.createElement("div");
   const location = document.createElement("p");
 
   postHeader.classList.add("post-header");
   userInfo.classList.add("user-info");
   avatar.classList.add("user-avatar");
-  //   locationContainer.classList.add("user-location");
+  // locationContainer.classList.add("user-location");
   name.classList.add("name");
   location.classList.add("location-name");
 
@@ -39,11 +81,28 @@ const createElements = (person) => {
   userInfo.append(name, location);
   postHeader.append(avatar, userInfo);
 
-  /* post */
+  return {
+    post,
+    postHeader,
+    image_post,
+  };
+}
+
+function createImagePost() {
   const image_post = document.createElement("img");
   image_post.classList.add("image-post");
-  image_post.src = "./assets/cat.jpg";
-  image_post.alt = "A kitten saw something that capt his atention";
+
+  images.forEach((_) => {
+    image_post.src = _.url;
+  });
+
+  // image_post.alt = photo.alt;
+
+  return image_post;
+}
+
+function createButtons() {
+  /* post buttons*/
 
   const buttons = document.createElement("div");
   buttons.classList.add("buttons");
@@ -69,26 +128,8 @@ const createElements = (person) => {
 
   buttons.append(likeBtn, commentBtn, shareBtn);
 
-  const totalLikes = document.createElement("p");
-  totalLikes.classList.add("total-likes");
-  totalLikes.textContent = `${Math.floor(Math.random() * 100)} likes`;
-
-  const div = document.createElement("div");
-  div.className = "username-photo-title";
-  const user_name = document.createElement("p");
-  user_name.textContent = `${person.login.username}`;
-
-  const title = document.createElement("p");
-  title.textContent = "My adorable kitten";
-  title.classList.add("commentary");
-  div.append(user_name, title);
-
-  user_name.classList.add("user-name");
-
-  post.append(postHeader, image_post, buttons, totalLikes, div);
-
-  posts.appendChild(post);
-};
+  return buttons;
+}
 
 const setDisplay = (persons) => {
   const { results } = persons;
@@ -97,4 +138,4 @@ const setDisplay = (persons) => {
   });
 };
 
-window.addEventListener('DOMContentLoaded',getUsers)
+window.addEventListener("DOMContentLoaded", getUsers);
